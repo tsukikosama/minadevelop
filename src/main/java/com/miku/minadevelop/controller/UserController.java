59724930bee41da9b6c3a7e58efb63ffd7eb272c
weilai <1083344129@qq.com>
2024-06-18 19:31:22 +0800

    添加了文件上传功能

--- src/main/java/com/miku/minadevelop/controller/UserController.java
+++ src/main/java/com/miku/minadevelop/controller/UserController.java
@@ -10,11 +10,14 @@ import cloud.tianai.captcha.spring.vo.ImageCaptchaVO;
 import cloud.tianai.captcha.validator.ImageCaptchaValidator;
 import cloud.tianai.captcha.validator.common.model.dto.ImageCaptchaTrack;
 import cloud.tianai.captcha.validator.impl.BasicCaptchaTrackValidator;
+import cn.dev33.satoken.stp.SaTokenInfo;
 import cn.dev33.satoken.stp.StpUtil;
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.miku.minadevelop.common.Result;
 import com.miku.minadevelop.entity.User;
 import com.miku.minadevelop.pojo.CheckData;
+import com.miku.minadevelop.pojo.UserPoJo;
 import com.miku.minadevelop.service.impl.UserServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -55,28 +58,48 @@ public class UserController {
      */
 
 //    @Captcha("SLIDER")
-    @PostMapping("/login")
-    public Result login(@RequestBody User user) {
+    @PostMapping("/login/{rememberMe}")
+    public Result login(@RequestBody User user,@PathVariable(value = "rememberMe",required = false) boolean rememberMe) {
         System.out.println(user);
         User one = userService.getOne(Wrappers.<User>lambdaQuery().eq(User::getAccount, user.getAccount()));
         System.out.println(one);
         if (one == null) {
             return Result.fail("账号不存在",406);
         }
-
         if (!one.getPassword().equals(user.getPassword())){
             return Result.fail("密码错误",406);
         }
-        StpUtil.login(one.getUid());
-        return Result.ok("登录成功");
+        /**
+         * 判断用户是否勾选了记住我的选项
+         */
+        if (rememberMe) {
+            StpUtil.login(one.getUid(),true);
+        }
+        StpUtil.login(one.getUid(),false);
+        /**
+         * 获取token数据
+         */
+        SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+        return Result.ok(tokenInfo);
     }
 
+    /**
+     * 通过用户id查询用户信息
+     */
+    @GetMapping("/{id}")
+    public Result getUserInfoById(@PathVariable("id") Long id) {
+        User user = userService.getById(id);
+        UserPoJo pojo = BeanUtil.copyProperties(user,UserPoJo.class);
+        if (pojo == null){
+            return Result.fail("用户不存在",406);
+        }
+        return Result.ok(pojo);
+    }
     /**
      * 注册
      */
     @PostMapping("/register")
     public Result register(@RequestBody User user) {
-
         return Result.ok();
     }
 
@@ -84,9 +107,10 @@ public class UserController {
      * 退出
      */
     @PostMapping("/logout")
-    public Result logout(@RequestBody User user) {
-
-        return Result.ok();
+    public Result logout(@RequestBody String uid) {
+        StpUtil.logout(uid);
+        log.info("用户{}注销成功",uid);
+        return Result.ok("退出登录成功");
     }
 
 
@@ -111,10 +135,8 @@ public class UserController {
             }
         }
         CaptchaResponse<ImageCaptchaVO> response = application.generateCaptcha(type);
-
         //把当前的response存入redis
         redisTemplate.opsForValue().set(response.getId(),response.getId());
-
         application.getImageCaptchaResourceManager();
         return response;
     }
@@ -124,12 +146,7 @@ public class UserController {
     public ApiResponse<?> checkCaptcha(@RequestBody CheckData data,
                                        HttpServletRequest request) {
           String s = redisTemplate.opsForValue().get(data.getId());
-
-
           ApiResponse<?> match = application.matching(s,data.getData());
-
-
-
         return match;
     }
 
